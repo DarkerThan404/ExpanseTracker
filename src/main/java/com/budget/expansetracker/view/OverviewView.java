3dcc4e7a9d37e4b66e3b4c59629f5e1429931d96
David Truhlář <truhlik24@gmail.com>
2023-06-01 17:08:40 +0200

    Rough OverviewView

--- src/main/java/com/budget/expansetracker/view/OverviewView.java
+++ src/main/java/com/budget/expansetracker/view/OverviewView.java
@@ -1,22 +1,68 @@
 package com.budget.expansetracker.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class OverviewView {
-    private VBox root;
+    private BorderPane root;
+    private Label balanceLabel;
+    private Text balanceText;
+    private VBox categoriesBox;
+    private VBox transactionsBox;
 
     public OverviewView(){
-        root = createView();
+        createView();
     }
 
     public Node getNode(){
         return root;
     }
 
-    private VBox createView() {
-        VBox vBox = new VBox();
-        return vBox;
+    private void createView() {
+        root = new BorderPane();
+        root.setPadding(new Insets(10));
+
+        // Create and configure the balance component
+        balanceLabel = new Label("Balance:");
+        balanceText = new Text("$5000"); // Replace with your balance value
+        HBox balanceBox = new HBox(balanceLabel, balanceText);
+        balanceBox.setAlignment(Pos.CENTER);
+
+        // Create and configure the categories component
+        categoriesBox = new VBox();
+        categoriesBox.setSpacing(5);
+        Label categoriesLabel = new Label("Categories:");
+        // Add category items to the categoriesBox
+        for (int i = 1; i <= 5; i++) {
+            Label categoryLabel = new Label("Category " + i);
+            categoriesBox.getChildren().add(categoryLabel);
+        }
+
+        // Create and configure the recent transactions component
+        transactionsBox = new VBox();
+        transactionsBox.setSpacing(5);
+        Label transactionsLabel = new Label("Recent Transactions:");
+        // Add transaction items to the transactionsBox
+        for (int i = 1; i <= 5; i++) {
+            Label transactionLabel = new Label("Transaction " + i);
+            transactionsBox.getChildren().add(transactionLabel);
+        }
+
+        // Add the components to the root VBox
+        //root.getChildren().addAll(balanceBox, categoriesLabel, categoriesBox, transactionsLabel, transactionsBox);
+        root.setTop(balanceBox);
+        root.setLeft(categoriesBox);
+        root.setRight(transactionsBox);
+    }
+
+    public void setBalanceText(String balance) {
+        balanceText.setText(balance);
     }
 
 }
