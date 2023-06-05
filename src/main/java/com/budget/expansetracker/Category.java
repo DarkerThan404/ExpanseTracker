beea63dc4bc00e02204cb4d1f85eb6c2e0eccdd5
David Truhlář <truhlik24@gmail.com>
2023-06-05 13:59:13 +0200

    Adds category fields to category class

--- src/main/java/com/budget/expansetracker/Category.java
+++ src/main/java/com/budget/expansetracker/Category.java
@@ -1,4 +1,18 @@
 package com.budget.expansetracker;
 
 public class Category {
+    private int ID;
+    private String name;
+    private double current;
+    private double goal;
+
+    public Category(int ID, String name, double current, double goal){
+        this.ID = ID;
+        this.name = name;
+        this.current = current;
+        this.goal = goal;
+    }
+
+
+
 }
