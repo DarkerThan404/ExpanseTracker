5533e259515c8bc2afaa1a048065289cb0a522ea
David Truhlář <truhlik24@gmail.com>
2023-06-05 13:54:23 +0200

    Adds transaction fields to transaction class

--- src/main/java/com/budget/expansetracker/Transaction.java
+++ src/main/java/com/budget/expansetracker/Transaction.java
@@ -1,4 +1,12 @@
 package com.budget.expansetracker;
 
+import java.time.LocalDate;
+
 public class Transaction {
+    private int ID;
+    private String name;
+    private LocalDate date;
+    private double amount;
+    private Category category;
+    private String description;
 }
