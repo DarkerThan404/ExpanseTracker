c122727c5074f60dcdc2ec3c83e573ebf02c3ded
David Truhlář <truhlik24@gmail.com>
2023-06-05 14:03:14 +0200

    Adds setter and getters to transaction fields in transaction class

--- src/main/java/com/budget/expansetracker/Transaction.java
+++ src/main/java/com/budget/expansetracker/Transaction.java
@@ -9,4 +9,63 @@ public class Transaction {
     private double amount;
     private Category category;
     private String description;
+
+    public Transaction(int ID, String name, LocalDate date, double amount, Category category, String description) {
+        this.ID = ID;
+        this.name = name;
+        this.date = date;
+        this.amount = amount;
+        this.category = category;
+        this.description = description;
+    }
+
+    // Getters
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Setters
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
