a41c33487a8fc292bd3c27625ebc21bfd1be41c3
David Truhlář <truhlik24@gmail.com>
2023-06-09 14:42:45 +0200

    Adds core methods to models

--- src/main/java/com/budget/expansetracker/model/CategoryModel.java
+++ src/main/java/com/budget/expansetracker/model/CategoryModel.java
@@ -1,4 +1,25 @@
 package com.budget.expansetracker.model;
 
+import com.budget.expansetracker.Category;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class CategoryModel {
+    private ObservableList<Category> categories;
+
+    public CategoryModel(){
+        categories = FXCollections.observableArrayList();
+    }
+
+    public void add(Category category){
+        categories.add(category);
+    }
+
+    public void remove(Category category){
+        categories.remove(category);
+    }
+
+    public ObservableList getCategories(){
+        return categories;
+    }
 }
--- src/main/java/com/budget/expansetracker/model/TransactionModel.java
+++ src/main/java/com/budget/expansetracker/model/TransactionModel.java
@@ -1,4 +1,25 @@
 package com.budget.expansetracker.model;
 
+import com.budget.expansetracker.Transaction;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class TransactionModel {
+    private ObservableList<Transaction> transactions;
+
+    public TransactionModel(){
+        transactions = FXCollections.observableArrayList();
+    }
+
+    public void add(Transaction transaction){
+        transactions.add(transaction);
+    }
+
+    public void remove(Transaction transaction){
+        transactions.remove(transaction);
+    }
+
+    public ObservableList getTransactions(){
+        return transactions;
+    }
 }
