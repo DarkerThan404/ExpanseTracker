06d39f27765d5535d4c35824b2e796c37fbfa7dc
David Truhlář <truhlik24@gmail.com>
2023-06-13 14:00:59 +0200

    Implements getCategories and getTransactions methods

--- src/main/java/com/budget/expansetracker/DataStorageManager.java
+++ src/main/java/com/budget/expansetracker/DataStorageManager.java
@@ -3,6 +3,11 @@ package com.budget.expansetracker;
 import com.budget.expansetracker.model.CategoryModel;
 import com.budget.expansetracker.model.TransactionModel;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,44 +19,80 @@ public class DataStorageManager {
     private TransactionModel transactions;
 
     public CategoryModel getCategories(){
+        if(categories == null){
+            try {
+                loadCategoriesFromFile();
+                System.out.println("Data loaded successfully.");
+            } catch (IOException e) {
+                System.out.println("Error loading data: " + e.getMessage());
+            }
+        }
         return categories;
     }
 
     public TransactionModel getTransactions(){
+        if(transactions == null){
+            try {
+                loadTransactionsFromFile();
+                System.out.println("Data loaded successfully.");
+            } catch (IOException e) {
+                System.out.println("Error loading data: " + e.getMessage());
+            }
+        }
         return transactions;
     }
 
-    public static void saveTransaction(Transaction transaction) {
-        // Logic to save a transaction to the transaction file
+    public void saveDataToFile() {
+        try {
+            saveCategoriesToFile();
+            saveTransactionsToFile();
+            System.out.println("Data saved successfully.");
+        } catch (IOException e) {
+            System.out.println("Error saving data: " + e.getMessage());
+        }
     }
 
-    public static List<Transaction> loadTransactions() {
-        // Logic to load transactions from the transaction file
-        return null;
+    private void saveCategoriesToFile() throws IOException {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(CATEGORY_FILE_PATH))) {
+            for (Category category : categories.getCategories()) {
+                writer.println(category.toCsv());
+            }
+        }
     }
 
-    public static void editTransaction(Transaction transaction) {
-        // Logic to edit a transaction in the transaction file
+    private void saveTransactionsToFile() throws IOException {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(TRANSACTION_FILE_PATH))) {
+            for (Transaction transaction : transactions.getTransactions()) {
+                writer.println(transaction.toCsv());
+            }
+        }
     }
 
-    public static void deleteTransaction(Transaction transaction) {
-        // Logic to delete a transaction from the transaction file
+    public void loadDataFromFile() {
+        try {
+            loadCategoriesFromFile();
+            loadTransactionsFromFile();
+            System.out.println("Data loaded successfully.");
+        } catch (IOException e) {
+            System.out.println("Error loading data: " + e.getMessage());
+        }
     }
 
-    public static void saveCategory(Category category) {
-        // Logic to save a category to the category file
+    private void loadCategoriesFromFile() throws IOException {
+        categories = new CategoryModel();
+        List<String> lines = Files.readAllLines(Paths.get(CATEGORY_FILE_PATH));
+        for (String line : lines) {
+            Category category = Category.fromCsv(line);
+            categories.add(category);
+        }
     }
 
-    public static List<Category> loadCategories() {
-        // Logic to load categories from the category file
-        return null;
-    }
-
-    public static void editCategory(Category category) {
-        // Logic to edit a category in the category file
-    }
-
-    public static void deleteCategory(Category category) {
-        // Logic to delete a category from the category file
+    private void loadTransactionsFromFile() throws IOException {
+        transactions = new TransactionModel();
+        List<String> lines = Files.readAllLines(Paths.get(TRANSACTION_FILE_PATH));
+        for (String line : lines) {
+            Transaction transaction = Transaction.fromCsv(line,categories);
+            transactions.add(transaction);
+        }
     }
 }
