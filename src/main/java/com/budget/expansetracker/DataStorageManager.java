bc09b7258cc9cbc14d89d994c600c4d9b3f27394
David Truhlář <truhlik24@gmail.com>
2023-06-13 15:58:40 +0200

    Adds data storage and basic handling mainly loading of data

--- src/main/java/com/budget/expansetracker/DataStorageManager.java
+++ src/main/java/com/budget/expansetracker/DataStorageManager.java
@@ -3,27 +3,30 @@ package com.budget.expansetracker;
 import com.budget.expansetracker.model.CategoryModel;
 import com.budget.expansetracker.model.TransactionModel;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class DataStorageManager {
-    private static final String TRANSACTION_FILE_PATH = "transactions.csv";
-    private static final String CATEGORY_FILE_PATH = "categories.csv";
+    private static final String DATA_DIRECTORY = "data";
+    private static final String TRANSACTION_FILE_PATH = DATA_DIRECTORY + File.separator + "transactions.csv";
+    private static final String CATEGORY_FILE_PATH = DATA_DIRECTORY + File.separator + "categories.csv";
 
     private CategoryModel categories;
     private TransactionModel transactions;
 
     public CategoryModel getCategoryModel(){
+        System.out.println(System.getProperty("user.dir"));
         if(categories == null){
             try {
                 loadCategoriesFromFile();
                 System.out.println("Data loaded successfully.");
-            } catch (IOException e) {
+            } catch (IOException | URISyntaxException e) {
                 System.out.println("Error loading data: " + e.getMessage());
             }
         }
@@ -35,7 +38,7 @@ public class DataStorageManager {
             try {
                 loadTransactionsFromFile();
                 System.out.println("Data loaded successfully.");
-            } catch (IOException e) {
+            } catch (IOException | URISyntaxException e) {
                 System.out.println("Error loading data: " + e.getMessage());
             }
         }
@@ -73,26 +76,31 @@ public class DataStorageManager {
             loadCategoriesFromFile();
             loadTransactionsFromFile();
             System.out.println("Data loaded successfully.");
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             System.out.println("Error loading data: " + e.getMessage());
         }
     }
 
-    private void loadCategoriesFromFile() throws IOException {
+    private void loadCategoriesFromFile() throws IOException, URISyntaxException {
         categories = new CategoryModel();
-        List<String> lines = Files.readAllLines(Paths.get(CATEGORY_FILE_PATH));
+        List<String> lines = Files.readAllLines(Paths.get(getDataFilePath(CATEGORY_FILE_PATH)));
         for (String line : lines) {
             Category category = Category.fromCsv(line);
             categories.add(category);
         }
     }
 
-    private void loadTransactionsFromFile() throws IOException {
+    private void loadTransactionsFromFile() throws IOException, URISyntaxException {
         transactions = new TransactionModel();
-        List<String> lines = Files.readAllLines(Paths.get(TRANSACTION_FILE_PATH));
+        List<String> lines = Files.readAllLines(Paths.get(getDataFilePath(TRANSACTION_FILE_PATH)));
         for (String line : lines) {
             Transaction transaction = Transaction.fromCsv(line,categories);
             transactions.add(transaction);
         }
+    }
+
+    private String getDataFilePath(String fileName) {
+        String workingDirectory = System.getProperty("user.dir");
+        return workingDirectory + File.separator + fileName;
     }
 }
