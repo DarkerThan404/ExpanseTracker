a9b7d3cae0ad30e6af33247a791b24f9594d40db
David Truhlář <truhlik24@gmail.com>
2023-06-27 13:13:23 +0200

    Implements validation for add transaction

--- src/main/java/com/budget/expansetracker/controllers/TransactionsController.java
+++ src/main/java/com/budget/expansetracker/controllers/TransactionsController.java
@@ -6,12 +6,15 @@ import com.budget.expansetracker.Transaction;
 import com.budget.expansetracker.model.CategoryModel;
 import com.budget.expansetracker.model.TransactionModel;
 import com.budget.expansetracker.view.TransactionsView;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -74,27 +77,78 @@ public class TransactionsController implements IController {
 
         TextField descriptionField = new TextField();
 
+        Label nameLabelError = new Label();
+        Label dateLabelError = new Label();
+        Label amountLabelError = new Label();
+        Label typeLabelError = new Label();
+        Label categoryLabelError = new Label();
+
+        nameLabelError.setTextFill(Color.RED);
+        dateLabelError.setTextFill(Color.RED);
+        amountLabelError.setTextFill(Color.RED);
+        typeLabelError.setTextFill(Color.RED);
+        categoryLabelError.setTextFill(Color.RED);
+
         GridPane grid = new GridPane();
         grid.setHgap(10);
         grid.setVgap(10);
 
-        grid.addRow(0, new Label("Name:"), nameField);
-        grid.addRow(1, new Label("Date:"), dateField);
+        int rowIdx = 0;
+
+        grid.addRow(rowIdx++, new Label("Name:"), nameField);
+        grid.addRow(rowIdx++, nameLabelError);
+
+
+        grid.addRow(rowIdx++, new Label("Date:"), dateField);
+        grid.addRow(rowIdx++, dateLabelError);
 
         HBox amountTypeBox = new HBox(10);
         amountTypeBox.getChildren().addAll(new Label("Amount:"), amountField, new Label("Type:"), typeComboBox);
-        grid.addRow(2, amountTypeBox);
+        grid.addRow(rowIdx++, amountTypeBox);
 
-        grid.addRow(3, new Label("Category:"), categoryComboBox);
-        grid.addRow(4, new Label("Description:"), descriptionField);
+        grid.addRow(rowIdx++, amountLabelError, typeLabelError);
+
+        grid.addRow(rowIdx++, new Label("Category:"), categoryComboBox);
+        grid.addRow(rowIdx++, categoryLabelError);
+
+        grid.addRow(rowIdx++, new Label("Description:"), descriptionField);
+
         dialog.getDialogPane().setContent(grid);
 
-        ButtonType addButton = new ButtonType("Add", ButtonBar.ButtonData.OK_DONE);
-        ButtonType cancelButton = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
-        dialog.getDialogPane().getButtonTypes().addAll(addButton, cancelButton);
+        ButtonType addButtonType = new ButtonType("Add", ButtonBar.ButtonData.OK_DONE);
+        ButtonType cancelButtonType = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+        dialog.getDialogPane().getButtonTypes().addAll(addButtonType, cancelButtonType);
+
+        // Disable the "Add" button by default
+        Button addButton = (Button) dialog.getDialogPane().lookupButton(addButtonType);
+        addButton.setDisable(true);
+
+        // Add a change listener to enable/disable the "Add" button
+        ChangeListener<Object> fieldsChangeListener = (observable, oldValue, newValue) -> {
+            boolean isNameValid = !nameField.getText().trim().isEmpty();
+            boolean isDateValid = dateField.getValue() != null;
+            boolean isAmountValid = amountField.getText().matches("^\\d*\\.?\\d+$");
+            boolean isTypeValid = typeComboBox.getValue() != null;
+            boolean isCategoryValid = categoryComboBox.getValue() != null;
+
+            addButton.setDisable(!isNameValid || !isDateValid || !isAmountValid || !isTypeValid || !isCategoryValid);
+
+            nameLabelError.setText(isNameValid ? "" : "Name is required");
+            dateLabelError.setText(isDateValid ? "" : "Date is required");
+            amountLabelError.setText(isAmountValid ? "" : "Amount must be a positive number");
+            typeLabelError.setText(isTypeValid ? "" : "Type is required");
+            categoryLabelError.setText(isCategoryValid ? "" : "Category is required");
+        };
+
+        // Register the change listener for all relevant fields
+        nameField.textProperty().addListener(fieldsChangeListener);
+        dateField.valueProperty().addListener(fieldsChangeListener);
+        amountField.textProperty().addListener(fieldsChangeListener);
+        typeComboBox.valueProperty().addListener(fieldsChangeListener);
+        categoryComboBox.valueProperty().addListener(fieldsChangeListener);
 
         dialog.setResultConverter(dialogButton -> {
-            if (dialogButton == addButton) {
+            if (dialogButton == addButtonType) {
                 String name = nameField.getText();
                 LocalDate date = dateField.getValue();
                 double amount = Double.parseDouble(amountField.getText());
