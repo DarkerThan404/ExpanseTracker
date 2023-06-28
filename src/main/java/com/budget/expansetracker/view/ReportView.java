6c15621449142c32459626eff3d0e21237ff826f
David Truhlář <truhlik24@gmail.com>
2023-06-28 16:07:45 +0200

    Adds montly expanse graph

--- src/main/java/com/budget/expansetracker/view/ReportView.java
+++ src/main/java/com/budget/expansetracker/view/ReportView.java
@@ -8,12 +8,15 @@ import com.budget.expansetracker.model.TransactionModel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
-import javafx.scene.chart.PieChart;
+import javafx.scene.chart.*;
 import javafx.scene.layout.VBox;
 
+import java.time.YearMonth;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class ReportView implements IView {
 
@@ -37,7 +40,7 @@ public class ReportView implements IView {
 
     private void createView(){
         root = new VBox();
-
+        createMonthlyTrendsChart();
         // Create the pie chart and configure it
         PieChart pieChart = createPieChart();
         root.getChildren().add(pieChart);
@@ -76,6 +79,54 @@ public class ReportView implements IView {
         }
 
         return categoryAmounts;
+    }
+
+    private void createMonthlyTrendsChart() {
+        // Retrieve transactions
+        List<Transaction> transactions = transactionModel.getTransactions();
+
+        // Group transactions by month
+        Map<YearMonth, List<Transaction>> transactionsByMonth = transactions.stream()
+                .collect(Collectors.groupingBy(transaction -> YearMonth.from(transaction.getDate())));
+
+        // Calculate total expenses for each month
+        Map<YearMonth, Double> monthlyExpenses = new TreeMap<>();
+        for (Map.Entry<YearMonth, List<Transaction>> entry : transactionsByMonth.entrySet()) {
+            YearMonth month = entry.getKey();
+            List<Transaction> monthTransactions = entry.getValue();
+            double totalExpense = monthTransactions.stream()
+                    .filter(transaction -> transaction.getType() == Transaction.TransactionType.EXPENSE)
+                    .mapToDouble(Transaction::getAmount)
+                    .sum();
+            monthlyExpenses.put(month, totalExpense);
+        }
+
+        // Create a line chart
+        LineChart<String, Number> lineChart = new LineChart<>(new CategoryAxis(), new NumberAxis());
+
+        // Set chart properties
+        lineChart.setTitle("Monthly Expense Trends");
+        lineChart.getXAxis().setLabel("Month");
+        lineChart.getYAxis().setLabel("Total Expense");
+
+        // Create series for data
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Expenses");
+
+        // Add data to the series
+        for (Map.Entry<YearMonth, Double> entry : monthlyExpenses.entrySet()) {
+            YearMonth month = entry.getKey();
+            double totalExpense = entry.getValue();
+            series.getData().add(new XYChart.Data<>(month.toString(), totalExpense));
+        }
+
+        // Add the series to the line chart
+        lineChart.getData().add(series);
+
+        // Customize other chart properties as desired
+
+        // Add the line chart to the root container
+        root.getChildren().add(lineChart);
     }
 
 
