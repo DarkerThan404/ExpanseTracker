7d8c0f374bbfec72df5a21b25a8c1bc843197051
David Truhlář <truhlik24@gmail.com>
2023-07-06 16:02:52 +0200

    Adds more categories

--- src/main/java/com/budget/expansetracker/view/ReportView.java
+++ src/main/java/com/budget/expansetracker/view/ReportView.java
@@ -14,7 +14,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
 import java.time.Month;
 import java.time.YearMonth;
