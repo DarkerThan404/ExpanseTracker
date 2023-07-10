67cd53dd5c807f047c85099c6bc79631e2fb2567
David Truhlář <truhlik24@gmail.com>
2023-07-10 12:18:34 +0200

    Deletes unused helper function

--- src/main/java/com/budget/expansetracker/DataStorageManager.java
+++ src/main/java/com/budget/expansetracker/DataStorageManager.java
@@ -165,19 +165,4 @@ public class DataStorageManager {
         }
         return total;
     }
-
-    public double calculateInitialBalance() {
-        double incomeTotal = 0.0;
-        double expenseTotal = 0.0;
-
-        for (Transaction transaction : transactions.getTransactions()) {
-            if (transaction.getType() == Transaction.TransactionType.INCOME) {
-                incomeTotal += transaction.getAmount();
-            } else if (transaction.getType() == Transaction.TransactionType.EXPENSE) {
-                expenseTotal += transaction.getAmount();
-            }
-        }
-
-        return incomeTotal - expenseTotal;
-    }
 }
